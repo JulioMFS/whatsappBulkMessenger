933019ede4a53a4aef96e5ef7d511ac8f8a824d8
JulioMFS <sanfonaj@gmail.com>
2022-12-16 22:56:33 +0000

    whatsapp Bulk 5

--- automator.py
+++ automator.py
@@ -1,3 +1,4 @@
+#--------------------- Whatsapp Automate ----------------#
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait

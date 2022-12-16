29e33138e648494e1d548a1f78293cc8fc73d724
JulioMFS <sanfonaj@gmail.com>
2022-12-16 21:53:21 +0000

    whatsapp Bulk 4

--- automator.py
+++ automator.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
+from time import  sleep
 from urllib.parse import quote
 import os
 
